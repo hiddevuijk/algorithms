f43df7069dfe920d6280005783763556e7b1612a
hiddevuijk <hdvuijk@gmail.com>
2020-03-07 20:53:41 +0100

    finished quick find union find

--- week1/main.cpp
+++ week1/main.cpp
@@ -5,19 +5,42 @@
 #include <vector>
 #include <string>
 
+#include <fstream>
+
 using namespace std;
+
+void read_from_file(string iname)
+{
+	ifstream ist(iname);
+	if( !ist ) cerr << "File does not exist \n";
+	int N;
+	ist >> N;
+
+}
+
 
 int main()
 {
 
+	ifstream ist("infile.txt");
+	if( !ist ) cerr << "File does not exit! \n";
 
+	int N;
+	ist >> N;
+	QuickFindUF qf_uf(N);
 
+	int p,q;
+	while( ist >> p >> q)
+		qf_uf.add_union(p,q);
 
+	vector<int> id = qf_uf.get_id();
+	for(int i=0; i<id.size(); i++)
+		cout << i <<' ';
+	cout << '\n';
 
-
-
-
-
+	for(int i=0; i<id.size(); i++)
+		cout << id[i] << ' ';
+	cout << '\n';
 
 	return 0;
 }
--- week1/union_find.h
+++ week1/union_find.h
@@ -1,18 +1,47 @@
 #ifndef GUARD_UNION_FIND_H
 #define GUARD_UNION_FIND_H
 
-class QF_UF {
-public:
-	QF_UF(int N);
+#include <vector>
+#include <iostream>
 
-	void add_union(int p, int q);
-	bool connected(int p, int q);
+
+class QuickFindUF {
+public:
+	QuickFindUF(int N)
+	: N(N), id(N)
+	{
+		for( int i=0; i<N; ++i)
+			id[i] = i;	
+	}
+
+
+	void add_union(int p, int q)
+	{
+		int pid = id[p];
+		int qid = id[q];
+		for( int i=0; i<N; ++i)
+			if(id[i] == pid) id[i] = qid;
+	}
+
+	bool connected(int p, int q)
+	{
+		return id[p] == id[q];
+	}
+
 	int find(int p);
 	int count();
 
+	std::vector<int> get_id(){ return id;}
 
+	
+
+private:
+	unsigned int N;
+	std::vector<int> id;	
 
 };
+
+
 
 
 
