b8da0d388f4af1092d6f69614c896c03d2025836
hiddevuijk <hdvuijk@gmail.com>
2020-03-08 09:53:15 +0100

    finished

--- week1/union_find.h
+++ week1/union_find.h
@@ -5,9 +5,9 @@
 #include <iostream>
 
 
-class QuickFindUF {
+class QuickUnionUF {
 public:
-	QuickFindUF(int N)
+	QuickUnionUF(int N)
 	: N(N), id(N)
 	{
 		for( int i=0; i<N; ++i)
@@ -15,17 +15,51 @@ public:
 	}
 
 
+
+
+	std::vector<int> get_id(){ return id;}
+
+private:
+	unsigned int N;
+	std::vector<int> id;	
+
+};
+
+
+
+
+class QuickFindUF {
+public:
+	QuickFindUF(int N)
+	: N(N), id(N), sz(N)
+	{
+		for( int i=0; i<N; ++i) {
+			id[i] = i;	
+			sz[i] = 1;	
+		}
+	}
+
+
 	void add_union(int p, int q)
 	{
-		int pid = id[p];
-		int qid = id[q];
-		for( int i=0; i<N; ++i)
-			if(id[i] == pid) id[i] = qid;
+		int i = root(p);
+		int j = root(q);
+		//id[i] = j;
+
+		if(i == j) return;
+
+		if( sz[i] < sz[j] ) {
+			id[i] = j;
+			sz[j] += sz[i];
+		} else {
+			id[j] = i;
+			sz[i] += sz[j];
+		}
 	}
 
 	bool connected(int p, int q)
 	{
-		return id[p] == id[q];
+		return root(p) == root(q);
 	}
 
 	int find(int p);
@@ -38,7 +72,19 @@ public:
 private:
 	unsigned int N;
 	std::vector<int> id;	
+	std::vector<int> sz;
 
+	int root(int i)
+	{
+		//while( i != id[i] ) i = id[i];
+
+		// flatten
+		while( i != id[i] ) {
+			id[i] = id[id[i]];
+			i = id[i];
+		}
+		return i;
+	}
 };
 
 
