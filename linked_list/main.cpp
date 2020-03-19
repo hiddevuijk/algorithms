320b04c9911e1d2072f6def1d97c841e19253ace
hiddevuijk <hdvuijk@gmail.com>
2020-03-19 12:35:58 +0100

    no maj changes

--- linked_list/main.cpp
+++ linked_list/main.cpp
@@ -2,13 +2,31 @@
 #include "linked_list.h"
 
 #include <iostream>
+#include <vector>
 #include <string>
 
 using namespace std;
 
 int main()
 {
+	
+	List<int> list = {0,1,2,3};
 
+
+	List<int> list2 = {10,11,12,13,14};
+	list2 = list;
+	
+
+	cout << endl;
+	while( list.begin() != list.end() ) {
+		cout << list.pop_front() << endl;
+	}
+
+	while( list2.begin() != list2.end() ) {
+		cout << list2.pop_front() << endl;
+	}
+
+		
 	return 0;
 
 }
