5081fedc57fcf1b63d4c229521d921d2b7058f2a
hiddevuijk <hiddevuijk@gmail.com>
2020-08-03 11:36:42 +0200

    finished queue (linked list implementation) with iterator (check API)

--- queue/main.cpp
+++ queue/main.cpp
@@ -9,7 +9,16 @@ using namespace std;
 
 int main()
 {
+    Queue<int> q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
 
-
+    Queue<int>::Iterator it = q.begin();
+    Queue<int>::Iterator end = q.end();
+    while( it != end ) {
+        cout << *it << endl;
+        ++it;
+    }
 	return 0;
 }
