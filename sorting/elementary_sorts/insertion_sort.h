ff48a0e64ed00dc7067b806cb5ae4c4c33039170
hiddevuijk <hiddevuijk@gmail.com>
2020-08-06 14:09:36 +0200

    fixed some errors

--- sorting/elementary_sorts/insertion_sort.h
+++ sorting/elementary_sorts/insertion_sort.h
@@ -5,8 +5,8 @@ template<class Item>
 void insertion(Item a[], int l, int r)
 {
     Item temp;
-    for( int i=1; i<=r; ++i) {
-       for( int j=i; j>0 && ( a[j] <  a[j-1] ); --j) {
+    for( int i=l; i<=r; ++i) {
+       for( int j=i; j>l && ( a[j] <  a[j-1] ); --j) {
             temp = a[j];
             a[j] = a[j-1];
             a[j-1] = temp;
