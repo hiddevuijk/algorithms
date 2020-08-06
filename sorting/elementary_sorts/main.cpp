61484c2a194c8e0292d01ff3a4d3efbf60f730a7
hiddevuijk <hiddevuijk@gmail.com>
2020-08-06 14:10:11 +0200

    some minor changes

--- sorting/elementary_sorts/main.cpp
+++ sorting/elementary_sorts/main.cpp
@@ -1,6 +1,7 @@
 
 #include "selection_sort.h"
 #include "insertion_sort.h"
+#include "merge_sort.h"
 
 #include <string>
 #include <vector>
@@ -9,16 +10,29 @@
 
 using namespace std;
 
+template< class Item>
+bool sorted(Item a[], int N) {
+    for(int i=1;i<N; ++i)
+        if( a[i-1] > a[i] ) return false;
+    return true;
+}
+
 int main()
 {
-    int N = 100000;
+    int N = 10000000;
     int *a = new int[N];
-    for(int i=0;i<N; ++i) a[i] = N-i;
-    selection(a,0,N-1); 
-    //insertion(a,0,N-1); 
 
-    //for(int i=0;i<N;++i) cout << a[i] << endl;
-    cout << a[0] << endl;
+    for(int i=0;i<N; ++i) 
+        a[i] =  (1- (int) (i%6) )*i ;
+
+    for(int i=0;i<10; ++i) cout << a[i] << endl;
+    merge_sort(a,0,N);
+    cout << endl;
+    for(int i=0;i<10; ++i) cout << a[i] << endl;
+
+    cout << endl;
+    cout << sorted(a, N) << endl;
+
     return 0;
 }
 
