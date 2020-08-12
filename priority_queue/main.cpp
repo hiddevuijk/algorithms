b741169c7c4844e15dcbb3d40b8d916574ecc41e
hiddevuijk <hiddevuijk@gmail.com>
2020-08-12 11:43:16 +0200

    finished pq

--- priority_queue/main.cpp
+++ priority_queue/main.cpp
@@ -11,8 +11,24 @@ using namespace std;
 int main()
 {
 
+    PQ<int> pq(100); 
 
+    int i;
+    char c;
+    for(;;) {
 
+        cout << "Give me an integer: ";
+        cin >> i;
+        if( i < 0) {
+            cout << "The largest int is: ";
+            if( !pq.empty() ) {
+                cout << pq.getmax() << endl;
+            } else {
+                cout << "Queue is empty" << endl;
+            }
+        } else { pq.insert(i); } 
+
+    }
 
     return 0;
 }
--- priority_queue/priority_queue.h
+++ priority_queue/priority_queue.h
@@ -7,12 +7,20 @@ public:
     PQ(int maxN)
         { pq = new Item[maxN+1]; N=0; }
 
-    bool empty() const
-        { return N==0; }
+    bool empty() const { return N==0; }
+    int size() const { return N; }
 
-    void insert(Item item);    
+    void insert(Item item)
+        { pq[++N] = item; swim(N); }
 
-    Item getmax();
+    Item getmax()
+        {
+            Item max = pq[1];
+            exch(1,N--);
+            sink(1);
+            return max;
+        }
+            
 
 
 
@@ -29,14 +37,18 @@ private:
     void swim(int k)
         {
             while( k>1 && less(k/2,k) )
-            { exch(k/2, k); k /= 2; }
+                { exch(k/2, k); k /= 2; }
         }
 
 
     void sink(int k)
         {
             while( 2*k <= N ){
-                
+                int j = 2*k;
+                if( j < N && less(j,j+1) ) ++j;
+                if( !less(k,j) ) break;
+                exch(k,j);
+                k = j;
             }
 
         }
