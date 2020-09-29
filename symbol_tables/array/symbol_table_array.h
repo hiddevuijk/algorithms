65212ecdedd25ecd99459f27720545aed236070c
hiddevuijk <hiddevuijk@gmail.com>
2020-09-29 09:59:11 +0200

    cont. ST

--- symbol_tables/array/symbol_table_array.h
+++ symbol_tables/array/symbol_table_array.h
@@ -1,5 +1,24 @@
 #ifndef GUARD_SYMBOL_TABLE_ARRAY_U
 #define GUARD_SYMBOL_TABLE_ARRAY_U
 
+template< class Key, class Value>
+class ST {
+public:
+    ST();
+
+    void put(Key key, Value val);
+    Value get(Key key) const; 
+    void del(Key key);
+    bool contains(Key key) const;
+    bool isEmpty() const { return N == 0; }
+    int size() const { return N; }
+    // iterable ...
+
+private:
+   int N; 
+
+
+};
+
 
 #endif
